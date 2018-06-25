8f789561d1d704535cdab247203f1064ee7ec65c
Marcelo Martínez <marcelo2m1097@gmail.com>
2018-06-24 19:35:59 -0600

    Added new table for rates of users.

--- app/models/user.go
+++ app/models/user.go
@@ -8,6 +8,8 @@ type User struct {				//Usuario
 	IdGenderUser         uint   //fk vinculada con el catálogo de género - idGeneroUsuario
 	IdAcademicLevelUser uint   //fk vinculada con el catálogo de nivel educativo - idNivelEducativoUsuario
 	IdRoleUser            uint   //fk vinculada con el catálogo de rol - idRolUsuario
+	IdRateUser			uint	//fk vinculada con el catálogo de rate - idCalificacionUsuario
+	UserPhoto			string `gorm:"type:text"`	//fotoUsuario
 	UserName           string `gorm:"type:varchar(150);not null"` //nombreUsuario
 	UserAge             uint8 //edadUsuario
 	UserEmail           string `gorm:"type:varchar(100);not null;unique"` //correoUsuario
