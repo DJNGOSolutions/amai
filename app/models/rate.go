311d3f71eded72cb3133d62fd5b6a8861d33be2c
Marcelo Martínez <marcelo2m1097@gmail.com>
2018-07-07 10:31:10 -0600

    Rate table passed from being catalogue to multivalued

--- app/models/rate.go
+++ app/models/rate.go
@@ -2,9 +2,10 @@ package models
 
 import "github.com/jinzhu/gorm"
 
-type Rate_User struct {
+type RatexUser struct {
 	//Esta estructura es un catálogo
 	gorm.Model
-	IdRate_User 	uint   `gorm:"primary_key"`				//idRole_User
-	Rate           	string `gorm:"type:varchar(20);unique"` //Rol
+	IdRatexUser 	uint   	`gorm:"primary_key"`				//idRole_User
+	IdUser 			uint										//idUsuario
+	Rate           	string 	`gorm:"type:varchar(20);unique"` 	//Rol
 }
