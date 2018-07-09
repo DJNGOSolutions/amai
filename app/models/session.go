73a9d9a56f26187969aaac7a20c7dddb4486f6d4
Ronald Vega <ronald.vega48@gmail.com>
2018-07-08 18:58:47 -0600

    sessions table now has a new relation with classrooms table

--- app/models/session.go
+++ app/models/session.go
@@ -5,6 +5,7 @@ import "github.com/jinzhu/gorm"
 type Session struct { //Cita
 	gorm.Model
 	IdSessionPlace   uint   //fk vinculada con el catálogo de lugares - idLugarSession
+	IdClassroom		uint
 	IdSubject        uint   //fk vinculada con la tabla de Materias - idMateria
 	IdTopic          uint   //fk vinculada con la tabla Temas - idTopic
 	IdTypeSession    uint   //fk vinculada con el catálogo de tipo de cita - idTipoCita
