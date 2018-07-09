c459bbe1603fe975c57e0a01d27626e38a1af974
Ronald Vega <ronald.vega48@gmail.com>
2018-07-08 19:11:32 -0600

    Modifications on some tables

--- app/models/topic.go
+++ app/models/topic.go
@@ -5,5 +5,6 @@ import "github.com/jinzhu/gorm"
 type Topic struct {
 	//Tema
 	gorm.Model
-	TopicName string `gorm:"type:varchar(75);unique"` //NombreTema
+	IdSubject	uint
+	TopicName	string `gorm:"type:varchar(75);unique"` //NombreTema
 }
