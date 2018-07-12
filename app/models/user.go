66af24806d8f2d1523c111fbf56c087fad1ccc38
Marcelo Martínez <marcelo2m1097@gmail.com>
2018-07-12 00:44:46 -0600

    Added field for the name of the student

--- app/models/user.go
+++ app/models/user.go
@@ -7,6 +7,7 @@ type UserT struct {
 	gorm.Model
 	UserPhoto       string `gorm:"type:text"`                  //fotoUsuario
 	UserName        string `gorm:"type:varchar(150);not null"` //nombreUsuario
+	Name			string `gorm:"type:varchar(150);not null"`
 	UserInstitution string `gorm:"type:varchar(50)"`
 	UserPassword    []byte `gorm:"type:text"`
 	IdGoogle        uint   `gorm:"type:integer;unique"`
